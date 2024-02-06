2d000d69c53ed6231afcc368b93430e1dc6fed4f
pushkar33 <pushkargoyal9417@gmail.com>
2024-02-06 23:53:10 +0530

    Project cleanup for Spring Rest

--- practiceproject/src/main/java/com/pugapa/practiceproject/PracticeprojectApplication.java
+++ practiceproject/src/main/java/com/pugapa/practiceproject/PracticeprojectApplication.java
@@ -2,7 +2,6 @@ package com.pugapa.practiceproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Collections;
 
@@ -10,18 +9,8 @@ import java.util.Collections;
 public class PracticeprojectApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(PracticeprojectApplication.class,args);
 
-		// Setting active profile programatically
-		var app=new SpringApplication(PracticeprojectApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("spring.profiles.active","dev"));
-
-		ConfigurableApplicationContext context=app.run(args);
-		MyFirstService mfc=context.getBean(MyFirstService.class);
-		System.out.println(mfc.tellAStory());
-		System.out.println(mfc.getMyProp());
-		System.out.println(mfc.getMyIntProp());
-		System.out.println(mfc.getCustomProp());
-		System.out.println(mfc.getCustomProp2());
 	}
 
 }
